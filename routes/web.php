501be15d87c72e136ef8b9d356ce4cfe33359e91
Maya <muvita123@gmail.com>
2026-08-19 09:16:38 +0700

    simpan pekerjaan buletin

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('beranda');
 });
+
 Route::get('/profile', function () {
     return view('profile');
 });
+
+Route::get('/buletin/{slug?}', function ($slug = null) {
+    return view('buletin');
+})->name('buletin');
